0fc43cc435e36826d4a949b4bbd9820c8a0ab9d7
Jordan Olshevski <jordanolshevski@gmail.com>
2016-02-09 08:41:02 -0800

    Remove parent_image osfamily validation
    We should allow users to specify any OS family, since theoretically it
    could be implemented by resource type providers in another module.

--- lib/puppet/type/parent_image.rb
+++ lib/puppet/type/parent_image.rb
@@ -5,8 +5,5 @@ Puppet::Type.newtype(:parent_image) do
   end
 
   newparam(:osfamily) do
-    validate do |value|
-      raise Puppet::Error, "'#{value}' is not a supported osfamily for the dockerfile function. It currently only supports 'el'" unless value == 'el'
-    end
   end
 end
