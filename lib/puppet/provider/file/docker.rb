b670948b3f0312ccf0b807410651dc3afcbb5918
Jordan Olshevski <jordanolshevski@gmail.com>
2016-02-10 14:52:33 -0800

    Merge PR#1
    This commit integrates the functionality of PR#1 and adds unit test
    coverage.

---
+++ lib/puppet/provider/file/docker.rb
@@ -0,0 +1,9 @@
+Puppet::Type.type(:file).provide(:docker) do
+  confine :feature => :docker
+
+  def dockerfile_line(context)
+    raise Puppet::Error, "The File type docker provider requires that both the 'source' and 'path' parameters be provided." unless @resource[:path] && @resource[:source]
+
+    "ADD #{@resource[:path]} #{@resource[:source][0].split(':')[1]}"
+  end
+end
---
+++ spec/unit/puppet/provider/file/docker_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:file).provider(:docker) do
+  it "should raise an error if no source is provided" do
+    expect {
+      Puppet::Type.type(:file).new(
+        :provider => :docker,
+        :title    => '/tmp/test',
+        :ensure   => 'present',
+      ).provider.dockerfile_line(nil)
+    }.to raise_error(/parameters be provided/)
+  end
+
+  it "should raise an error if source is not an absolute path" do
+    expect {
+      Puppet::Type.type(:file).new(
+        :provider => :docker,
+        :title    => '/tmp/test',
+        :source   => ['file:foo'],
+        :ensure   => 'present',
+      ).provider.dockerfile_line(nil)
+    }.to raise_error(/Cannot use opaque URLs/)
+  end
+
+  it "should return the correct dockerfile line" do
+    line = Puppet::Type.type(:file).new(
+      :provider => :docker,
+      :title    => '/tmp/foo',
+      :source   => ['file:/tmp/bar'],
+      :ensure   => 'present',
+    ).provider.dockerfile_line(nil)
+
+    expect(line).to eq('ADD /tmp/foo /tmp/bar')
+  end
+end
